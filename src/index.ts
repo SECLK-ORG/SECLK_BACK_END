45c9d0a2c3ed2e76cfc621c55ff5b2a8e4e981d5
Avishka <avishkachanaka@gmail.com>
2024-07-22 13:50:55 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 const express = require('express');
 import logger from './utils/logger';
 import { connect } from './database/database';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './utils/swaggerConfig';
 import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || "3000";
-
+// Serve Swagger docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 
 
 app.listen(PORT, () => {
-  logger.info(`Server is running on port [${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
+  // console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Swagger docs are available at http://localhost:${PORT}/api-docs`);
   connect();
 });
---
+++ src/utils/swaggerConfig.ts
@@ -0,0 +1,27 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+// Swagger definition
+const swaggerDefinition = {
+  openapi: '3.0.0', // You can also use 'swagger: "2.0"' here
+  info: {
+    title: 'SE Consultant APIs',
+    version: '1.0.0',
+    description: 'SE Consultant APIs',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000', // Change to your server URL
+      description: 'Development server',
+    },
+  ],
+};
+
+// Options for the swagger docs
+const options = {
+  swaggerDefinition,
+  apis: ['./src/routes/*.ts'], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
